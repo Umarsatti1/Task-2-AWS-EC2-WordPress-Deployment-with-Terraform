0bc11d812c4d58443ba48ebb237ba842e52d6c7b
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2025-11-10 17:30:48 +0500

    Update user data script

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -7,13 +7,12 @@ sudo systemctl start apache2 #Optional
 sudo systemctl enable apache2 #Optional
 
 # Install PHP and dependencies
-sudo apt install php libapache2-mod-php php-mysql -y
 sudo apt install php libapache2-mod-php php-mysql php-xml php-mbstring php-curl php-zip -y
 
 # Install MySQL
 sudo apt install mysql-server -y
 
-#Configure MySQL
+# Configure MySQL database and user
 mysql -e "CREATE DATABASE wordpress;"
 mysql -e "CREATE USER 'umarsatti'@'localhost' IDENTIFIED BY 'P@ssw0rd';"
 mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'umarsatti'@'localhost';"
@@ -27,15 +26,15 @@ tar -xzf /tmp/latest.tar.gz -C /tmp
 rm -rf /var/www/html/*
 mv /tmp/wordpress/* /var/www/html/
 
-# Fix permissions
+# Change permissions
 chown -R www-data:www-data /var/www/html/
 chmod -R 755 /var/www/html/
 
-#Configure Wordpress to connect to MySQL database
+# Configure Wordpress to connect to MySQL database
 cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 sed -i "s/database_name_here/wordpress/" /var/www/html/wordpress/wp-config.php
 sed -i "s/username_here/umarsatti/" /var/www/html/wordpress/wp-config.php
 sed -i "s/password_here/P@ssw0rd/" /var/www/html/wordpress/wp-config.php
 
-#Restart Apache Web Server
+# Restart Apache
 systemctl restart apache2
